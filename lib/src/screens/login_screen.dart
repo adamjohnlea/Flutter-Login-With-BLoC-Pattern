34842ea0e63718cd2d99c29cd369c51b794f70fc
Adam Lea <imnogeekdptcom@me.com>
2018-07-07 19:55:01 -0600

    create initial login_screen

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -7,9 +7,41 @@ class LoginScreen extends StatelessWidget {
         margin: EdgeInsets.all(20.0),
         child: Column(
           children: <Widget>[
-            
+            emailField(),
+            passwordField(),
+            Container(margin: EdgeInsets.only(top: 20.0)),
+            submitButton(),
           ],
         ),
+      );
+    }
+
+    Widget emailField() {
+      return TextField(
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'you@example.com',
+          labelText: 'Email Address',
+        ),
+      );
+    }
+
+    Widget passwordField() {
+      return TextField(
+        decoration: InputDecoration(
+          hintText: 'Must contain 8 characters',
+          labelText: 'Password',
+        ),
+        obscureText: true,
+      );
+    }
+
+    Widget submitButton() {
+      return RaisedButton(
+        child: Text('Login'),
+        color: Colors.blue[700],
+        textColor: Colors.white,
+        onPressed: () {},
       );
     }
 }
